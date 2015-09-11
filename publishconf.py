40ae1bd908d9124e6cfe784a61532a169f957a9d
Matthew Gilbert <matthew.gilbert12@gmail.com>
2015-09-10 21:13:13 -0400

    changed github name

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://MatthewGilbert.github.io'
+SITEURL = 'https://matthewgilbert.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
